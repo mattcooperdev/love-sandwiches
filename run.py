1be471edbb2aa213195a0502596012f0ba38aaa9
YesCoops <coopandmgc@gmail.com>
2022-01-03 13:51:24 +0000

    Created get sales data function to obtain data from user input

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+
+def get_sales_data():
+    """
+    Get sales figures input from the User
+    """
+
+    print("Please enter sales data form the last market.")
+    print("Data should be six numbers, separated by commas")
+    print("Example : 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
+
