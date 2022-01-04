d4d7b911dbc7e9be1a940504facc89b7a70087e8
YesCoops <coopandmgc@gmail.com>
2022-01-04 15:04:29 +0000

    Collect sales data for last 5 days

--- run.py
+++ run.py
@@ -86,7 +86,7 @@ def update_worksheet(data, worksheet):
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully")
+    print(f"{worksheet} worksheet updated successfully\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -107,6 +107,20 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the 
+    data as lists
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5 :])
+    
+    return columns
+
 
 def main():
     """
@@ -119,5 +133,6 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
 
+sales_columns = get_last_5_entries_sales()
